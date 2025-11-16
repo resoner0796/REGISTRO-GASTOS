e4083cfd6e2399739399e93e77d1d41e4cca694e
resoner0796 <ulisees.luna96@gmail.com>
2025-11-16 01:10:54 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const URLS_TO_CACHE = [
     "/",
     "/index.html",
     "/manifest.json",
-    "/icon-192.png",
-    "/icon-512.png"
+    "/icon-192.PNG",
+    "/icon-512.PNG"
 ];
 
 // INSTALACIÓN (cache inicial)
